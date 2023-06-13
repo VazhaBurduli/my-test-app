ce83d3f71e9d96969de523dbfb7a1dadd807a752
VazhaBurduli <121959221+VazhaBurduli@users.noreply.github.com>
2023-06-13 15:52:36 +0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      {
+              <div class="myCard">
+              <div class="info">
+                <h2>Driver's Licence</h2>
+                <h3>Jane Miller</h3>
+                <img src="https://media.istockphoto.com/id/1223392906/photo/cute-young-success-happy-brunette-woman-is-driving-a-car.jpg?s=612x612&w=0&k=20&c=4NOttnNXkosv8pfLzw0l5bRT2qU08NcwbxLxTm5RcwE="></img>
+                <p>Citizenship: U.S. Citizen</p>
+                <p>Age: 32</p>
+                <p>Driving experience: 4 years</p>
+                <p>Licence number: 376876</p>
+              </div>
+            </div>
+      }
+
+
+    </div>
+  );
+}
+
+export default App;
